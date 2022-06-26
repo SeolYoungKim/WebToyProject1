bc9cfd0f5f4a2451ac30eb4d23e21f239ecdda48
SeolYoungKim <nasur4da@gmail.com>
2022-06-26 19:12:55 +0900

    login+cookie 기능 구현 복습

--- src/main/java/WebToyProject1/webService/web/controller/HomeController.java
+++ src/main/java/WebToyProject1/webService/web/controller/HomeController.java
@@ -1,8 +1,12 @@
 package WebToyProject1.webService.web.controller;
 
+import WebToyProject1.webService.domain.member.Member;
+import WebToyProject1.webService.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
+    private final MemberRepository memberRepository;
+
+//    @GetMapping
     public String home() {
         log.info("Home 정상 진입");
         return "home";
     }
 
+    @GetMapping
+    public String loginHome(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member findMember = memberRepository.findById(memberId);
+        if (findMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", findMember);
+        return "loginHome";
+    }
 }
--- src/main/java/WebToyProject1/webService/web/controller/LoginController.java
+++ src/main/java/WebToyProject1/webService/web/controller/LoginController.java
@@ -10,25 +10,25 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/login")
 public class LoginController {
 
     private final LoginService loginService;
 
-    @GetMapping
+    @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm) {
         return "login/loginForm";
     }
 
-    @PostMapping
-    public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult) {
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -40,6 +40,38 @@ public class LoginController {
             return "login/loginForm";
         }
 
+        // 로그인에 성공하면 쿠키를 부여하고, 새로운 화면으로 넘어가게 해주자.
+        Cookie cookie = new Cookie("memberId", String.valueOf(member.getId()));
+        response.addCookie(cookie);
+
         return "redirect:/";
     }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String memberId) {
+        Cookie cookie = new Cookie(memberId, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
